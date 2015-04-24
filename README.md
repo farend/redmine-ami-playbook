da18738181743b7814de982c1e40a354911bbb23
iwaishi <iwaishi@farend.jp>
2015-04-24 15:41:06 +0900

    First version.

---
+++ README.md
@@ -0,0 +1,59 @@
+# Redmine AMIの作り方
+
+## インスタンス作成
+
+AWSコンソール等にてAmazon Linuxのmicroインスタンスを作成する。
+
+## OSを最新状態にする
+
+* 一度インスタンスにログインし、 `sudo yum -y update` を実施する。
+* 再起動する。
+
+## 使用インスタンスの特定
+
+* `hosts.sample` を `hosts` として複製する。
+
+### hosts
+
+* ホストのIPアドレスを設定する。
+* `ansible_ssh_private_key_file` を正規のファイルパスにする。
+
+## バージョン及び設定等の定義
+
+* `group_vars/redmine.sample` を `groups_vars/redmine` として複製する。
+
+### redmine
+
+* `mysql_root_password` に **MySQLのrootのパスワード** を設定する。
+* `redmine_db_pass` に **db_redmineデータベースのuser_redmineアカウントのパスワード** を設定する。
+* `ruby_major` に **rubyのメジャーバージョン（小数点1位まで）** を設定する。
+* `ruby_version` に **rubyのバージョンのruby_major以外** を設定する。
+* `redmine_version` に **Redmineのバージョン** を設定する。
+* `redmine_parent` に **Redmineを配置するディレクトリ** を設定する。
+
+## インストール実行
+
+**`ansible-playbook -i hosts main.yml`** で実行
+
+## Redmine設定
+
+*<u>Ansibleの実行が終わったら</u>*、Redmineにadminでログインし、デフォルトのテーマを **farend_basic** に変更。
+
+## 操作ログ等の削除
+
+インスタンスにログインし、下記を実行。
+
+```
+$ export HISTSIZE=0
+$ history -c
+$ sudo su -
+# cd
+# sh delete.sh
+# rm delete.sh
+# export HISTSIZE=0
+# history -c
+```
+
+## イメージ作成
+
+ログなどが削除されたか確認し、この状態でAMIを作成
---
+++ roles/ami_work/templates/delete.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+service httpd stop
+service postfix stop
+service mysqld stop
+service rsyslog stop
+cp /dev/null /root/.ssh/authorized_keys
+cp /dev/null /root/.bash_history
+cp /dev/null /root/.mysql_history
+cp /dev/null /home/ec2-user/.ssh/authorized_keys
+cp /dev/null /home/ec2-user/.bash_history
+cp /dev/null /home/ec2-user/.mysql_history
+rm -rf /home/ec2-user/.ansible
+cp /dev/null /var/log/boot.log
+cp /dev/null /var/log/btmp
+cp /dev/null /var/log/cron
+cp /dev/null /var/log/dmesg
+cp /dev/null /var/log/dmesg.old
+cp /dev/null /var/log/lastlog
+cp /dev/null /var/log/maillog
+cp /dev/null /var/log/messages
+cp /dev/null /var/log/secure
+cp /dev/null /var/log/spooler
+cp /dev/null /var/log/tallylog
+cp /dev/null /var/log/wtmp
+cp /dev/null /var/log/yum.log
+cp /dev/null /var/log/audit/audit.log
+cp /dev/null /var/log/cloud-init.log
+cp /dev/null /var/log/cloud-init-output.log
+cp /dev/null /var/log/dracut.log
+cp /dev/null /var/log/mysqld.log
+cp /dev/null /var/log/mail/statistics
+cp /dev/null /var/log/httpd/access_log
+cp /dev/null /var/log/httpd/error_log
+rm -f /var/lib/redmine/config/initializers/secret_token.rb
+rm -f /var/lib/redmine/log/production.log
+rm -f /var/lib/redmine/files/*
